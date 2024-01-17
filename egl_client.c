fb9a0bef48442a8b72bd757a63da9276daf83e12
P-S-Y-S-U <mohan.gaming78@gmail.com>
2024-01-17 15:33:11 +0530

    triangle drawing done

--- egl_client.c
+++ egl_client.c
@@ -10,14 +10,45 @@
 
 struct ClientObjState;
 
+static const char* vertex_shader_text = 
+	"#version 300 es\n"
+	"out vec4 vertex_color;\n"
+	"const vec2 positions[3] = vec2[3](\n"
+		"vec2(0.0, -0.5),\n"
+		"vec2(0.5, 0.5),\n"
+		"vec2(-0.5, 0.5)\n"
+	");\n"
+	"const vec3 colors[3] = vec3[3](\n"
+		"vec3(1.0, 0.0, 0.0),\n"
+		"vec3(0.0, 1.0, 0.0),\n"
+		"vec3(0.0, 0.0, 1.0)\n"
+	");\n"
+	"void main() {\n"
+	" gl_Position = vec4( positions[gl_VertexID], 0.0, 1.0 );\n"
+	" vertex_color = vec4( colors[gl_VertexID], 1.0 );\n"
+	"}\n";
+
+static const char* frag_shader_text = 
+	"#version 300 es\n"
+	"precision mediump float;\n"
+	"in vec4 vertex_color;\n"
+	"out vec4 fragColor;\n"
+	"void main() {\n"
+	"  fragColor = vertex_color;\n"
+	"}\n";
+
 static uint32_t startTime = 0;
 
 static void wl_buffer_release( void* pData, struct wl_buffer* pWlBuffer );
 
 static void updateFrame_callback( void* pData, struct wl_callback* pFrameCallback, uint32_t time );
-static void recordGlCommands( uint32_t time );
+static void recordGlCommands( struct eglContext* pEglContext, uint32_t time );
 
 static void surface_configure_callback( void * pData, struct wl_callback* pCallback, uint32_t time );
+
+static void InitGLState();
+
+static GLuint createShader( const char* source, GLenum shaderType );
 
 struct ClientObjState
 {
@@ -66,7 +97,7 @@ static void updateFrame_callback( void* pData, struct wl_callback* pFrameCallbac
 	if( !pClientObjState->mbSurfaceConfigured )
 		return;
 
-	recordGlCommands( time );
+	recordGlCommands( &pClientObjState->mpEglContext, time );
 
 	pClientObjState->mpFrameCallback = wl_surface_frame( pClientObjState->mpWlSurface );
 	wl_callback_add_listener( pClientObjState->mpFrameCallback, &frame_listener, pClientObjState );
@@ -74,8 +105,9 @@ static void updateFrame_callback( void* pData, struct wl_callback* pFrameCallbac
 	SwapEGLBuffers( &pClientObjState->mpEglContext );
 }
 
-static void recordGlCommands( uint32_t time )
+static void recordGlCommands( struct eglContext* pEglContext, uint32_t time )
 {
+#if 1
 	if( startTime == 0 )
 		startTime = time;
 
@@ -85,9 +117,18 @@ static void recordGlCommands( uint32_t time )
 	float green = ( ( rand() % ( 255 - 1 + 1 ) ) + 1 ) / 255.0;
 	float blue = ( ( rand() % ( 255 - 1 + 1 ) ) + 1 ) / 255.0;
 	float alpha = ( ( rand() % ( 255 - 0 + 1 ) ) + 0 ) / 255.0;	
+#else
+	float red = 0.0;
+	float green = 0.0;
+	float blue = 0.0;
+	float alpha = 1.0;
+#endif 
+	glViewport(0, 0, pEglContext->mWindowWidth, pEglContext->mWindowHeight );
 
 	glClearColor(red, green, blue, alpha);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays( GL_TRIANGLES, 0, 3 );
 }
 
 static void surface_configure_callback( void* pData, struct wl_callback* pCallback, uint32_t time )
@@ -100,6 +141,56 @@ static void surface_configure_callback( void* pData, struct wl_callback* pCallba
 
 	if( pClientObj->mpFrameCallback == NULL )
 		updateFrame_callback( pClientObj, NULL, time );
+}
+
+static void InitGLState()
+{
+	GLuint frag, vert;
+	GLuint program;
+	GLint status;
+
+	frag = createShader( frag_shader_text, GL_FRAGMENT_SHADER);
+	vert = createShader( vertex_shader_text, GL_VERTEX_SHADER);
+
+	program = glCreateProgram();
+	glAttachShader(program, frag);
+	glAttachShader(program, vert);
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if (!status) {
+		char log[1000];
+		GLsizei len;
+		glGetProgramInfoLog(program, 1000, &len, log);
+		fprintf(stderr, "Error: linking:\n%*s\n", len, log);
+		exit(1);
+	}
+
+	glUseProgram(program);
+}
+
+static GLuint createShader( const char* source, GLenum shaderType )
+{
+	GLuint shader;
+	GLint status;
+
+	shader = glCreateShader(shaderType);
+
+	glShaderSource(shader, 1, (const char **) &source, NULL);
+	glCompileShader(shader);
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (!status) {
+		char log[1000];
+		GLsizei len;
+		glGetShaderInfoLog(shader, 1000, &len, log);
+		fprintf(stderr, "Error: compiling %s: %*s\n",
+			shaderType == GL_VERTEX_SHADER ? "vertex" : "fragment",
+			len, log);
+		exit(1);
+	}
+
+	return shader;
 }
 
 static void xdg_surface_configure(
@@ -130,7 +221,7 @@ static void xdg_toplevel_handle_configure(
 	pClientObjState->mpEglContext.mWindowWidth = width;
 	pClientObjState->mpEglContext.mWindowHeight = height;
 
-	// resize
+	// resizecontext_attribs
 	wl_egl_window_resize( pClientObjState->mpEglContext.mNativeWindow, width, height, 0, 0 );
 }
 
@@ -170,7 +261,7 @@ int main( int argc, const char* argv[] )
 	clientObjState.mpGlobalObjState = &gObjState;
 
 	InitEGLContext( &clientObjState.mpEglContext );
-
+	
     clientObjState.mpWlSurface = wl_compositor_create_surface(gObjState.mpCompositor);
 	if( !clientObjState.mpWlSurface )
 	{
@@ -194,6 +285,7 @@ int main( int argc, const char* argv[] )
 	const char* surfaceTitle = "EGL Client";
 
 	CreateEGLSurface( clientObjState.mpWlSurface, surfaceWidth, surfaceHeight, &clientObjState.mpEglContext );
+	InitGLState();
 
     clientObjState.mpXdgTopLevel = xdg_surface_get_toplevel( clientObjState.mpXdgSurface );
 	AssignXDGToplevelListener(clientObjState.mpXdgTopLevel, &clientObjState);
